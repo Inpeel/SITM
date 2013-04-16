1705ff0520947e9b261ba409d176ebdc9f9eb9dc
CHATELAIN Nicolas <nchatelain@intechinfo.fr>
2013-04-16 15:19:34 +0200

    Ajout du fichier dhcp_server et mise a jour du scanner.

--- Sources/Functions/net_scan.pl
+++ Sources/Functions/net_scan.pl
@@ -7,25 +7,25 @@ my $ipfin = "10.8.111.255"; # Adresse IP de fin ( Ipdebut => IPfin = range analy
 my $currentip; #IP actuelle lors du scan
 my ($a,$b,$c,$d) = split(/\./, $ipdebut); # =(10.8.96.0 => 10.x.x.x)
 
-do # Boucle affichant la liste des adresses IP et parcourant une liste du début à la fin
+do
 {
-print "$a.$b.$c.$d\n";
-$currentip = "$a.$b.$c.$d";
-$d++;
-if ($d == 256)
-{
-$c++;
-$d = 0;
-}
-if ($c == 256)
-{
-$b++;
-$c =0;
-}
-if ($b == 256)
-{
-$a++;
-$b = 0;
-}
+	print "$a.$b.$c.$d\n";
+	$currentip = "$a.$b.$c.$d";
+	$d++;
+	if ($d == 256)
+	{
+		$c++;
+		$d = 0;
+	}
+	if ($c == 256)
+	{
+		$b++;
+		$c =0;
+	}
+	if ($b == 256)
+	{
+		$a++;
+		$b = 0;
+	}
 
-}while ($currentip ne $ipfin); # on défini la fin du scan lorsque l'IP scanné est égal à l'adresse IP de fin 
+} while ($currentip ne $ipfin);

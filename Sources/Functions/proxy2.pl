e25c8d748e91541b3352c8da732d2509fc0ef296
Alban <inquel@intechinfo.fr>
2013-06-19 14:41:50 +0200

    mise en place de fonctions, amélioration lors de 302 et 301 code

--- Sources/Functions/proxy2.pl
+++ Sources/Functions/proxy2.pl
@@ -4,13 +4,18 @@
 use strict;
 use warnings;
 
+use Convert::Scalar;
+use Encode;
+use HTML::Entities;
+use IO::Compress::Gzip qw(gzip $GzipError);
 use URI::Find;
 use URI::Escape;
+use URI::Encode qw(uri_encode uri_decode);
 use HTTP::Daemon;
 use LWP::UserAgent;
 use Crypt::SSLeay;
 use LWP::Protocol::https;
-my $tmp, @cache_cookie=();
+my @cache_cookie=();
 my @cache;
 my $pa = LWP::UserAgent->new;
 my $ua = LWP::UserAgent->new(keep_alive => 5, requests_redirectable => []);
@@ -19,12 +24,7 @@ my $http = HTTP::Daemon->new(
 	reuse => 1
 ) || die;
 print "[Proxy URL:", $http->url, "]\n";
-#BEGIN {
-	#$Net::HTTPS::SSL_SOCKET_CLASS = "Net::SSL";
-	#$ENV{HTTPS_PROXY} = '192.168.228.156:80';
-#}
-# Avoid dying from browser cancel
-#$SIG{PIPE} = 'IGNORE';
+
 
 
 print "En attente d'un client..\n";
@@ -35,13 +35,15 @@ while (my $c = $http->accept) {
 	print "Requête d'un nouveau client..\n"; 
 
         while (my $request = $c->get_request) {
-        	#print("Request Host : ".$request->header("Host") . "\n"); 
-        	#print("Request URI : ".$request->uri . "\n");
-        	#print("Modified URI : http://".$request->header("Host").$request->uri."\n");
         	$request->uri("http://".$request->header("Host").$request->uri);
-			#print "LIEN REQUETE : ".$request->uri."\n";
-			
-			
+			#my $tmp_http = $request;
+			#my $encoding_http;
+			#($tmp_http, $encoding_http) = decode_content($tmp_http);
+			#$tmp_http = replace_http($tmp_http);
+			#$tmp_http->encode($encoding_http);
+			#$request = $tmp_http;
+			#print "REQUETE TRANSFORME\n\n".$tmp_http->as_string."\n\n";
+
 			RESEND:
 			print "Envoi requete\n";
 			@{ $ua->requests_redirectable } = ();
@@ -53,125 +55,177 @@ while (my $c = $http->accept) {
 			CHECK_CODE:
 			print "LE STATUT DE LA REQUETE EST : ".$response->status_line."\n";
 			print "Code de la requete :".$response->code."\n";
+			open (MODIFIED, ">>tab.txt");
 			
 			if($response->code == 301){
-				print "Checking new location...\n";
+				$response = redirect_301($request, $response);
+				print "DANS BOUCLE 301\n\n";
+				goto NEXT;
+			}elsif($response->code == 302){
+				$response = redirect_302($request, $response);
+				goto NEXT;
+			}else{
+				NEXT:
+				print "PASSE\n\n";
+				my $tmp = $response;
+				my $encoding;
+				my $send;
+				($tmp, $encoding) = decode_content($tmp);
+				print "ENCODING IS $encoding\n\n";
+				print "DECODE\n\n";
+				$tmp = replace_https($tmp, $response);
+				print "REPLACE\n\n";
+				$send = pack_response($tmp, $response, $encoding);
+				print "SEND\n\n";
 				
-				$request->uri("");
-				$request->uri($response->header("Location").$request->uri);
-				#if($response = $ua->get("https://www.google.fr")){
-				#}else{
-				#	die "bleme de connexion https\n";
-				#}
- 				#$response = $ua->get("http://www.orange.fr");
- 				#print "Reponse http apres code 301 : ".$response->decoded_content."\n";
-				#open (MODIFIED, ">>tab.txt");
-				#print MODIFIED "Reponse http apres code 301\n\n";
-				#print MODIFIED $response->decoded_content."\n";
-				#close MODIFIED;
-				print "Renvoi de la requete : 301\n";
-				print "URL envoyé : ".$request->uri."\n";
-				push
-				if($response = $ua->request( $request )){
-					print "Requete 301 : retransmise\n";
-					#open (MODIFIED, ">>tab.txt");
-					#print MODIFIED $request->as_string."\n";
-					#print MODIFIED $response->as_string."\n";
-					#close MODIFIED;
-					print "COOKIE dans la boucle code : ".$response->header("Set-Cookie")."\n";
-					goto CHECK_CODE;
-				}
-			}
-
-			if($response->code == 302){
-				print "Checking new location...\n";
-				print "Renvoi de la requete : 302\n";
-				
-				#if($response->header("Location") =~ /(((https:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-]*)?\??(?:[\-\+=&;%@\.\w]*)#?(?:[\.\!\/\\\w]*))?)/){
-					#$response->header("Location") =~ s/https/http/;
-					$request->uri("");
-					$request->uri($response->header("Location").$request->uri);
-					print "Renvoi de la requete : 302\n";
-					#$request->uri($response->header("Location").$request->uri);
-					print "URL envoyé : ".$request->uri."\n";
-					if($response = $ua->request( $request )){
-						print "Requete 302 : retransmise\n";
-						open (MODIFIED, ">>tab.txt");
-						print MODIFIED $request->as_string."\n";
-						print MODIFIED $response->as_string."\n";
-						close MODIFIED;
-						goto CHECK_CODE;
-					}
-			}
-			
-			
-
-			#if ($response->content =~ m/https:\/\//){
-				#print "https trouvé!!\n\n\n\n\n";
-				
-
-				#if(my $content_encoding = $response->header("Content-Encoding")){
-				#	print "La methode d encodage est  :  $content_encoding\n\n\n\n";
-				#	$tmp = $response->decoded_content($content_encoding);
-				#}else{
-				#	$tmp = $response->decoded_content("UTF-8");
-				#}
-				$tmp = $response->decoded_content;
-				open (SOURCE, ">>source.html");
-				print SOURCE $tmp;
-				close SOURCE;
-				
-				
-				
-				open (MODIFIED, ">>modified.html");
-				if($tmp =~ /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-]*)?\??(?:[\-\+=&;%@\.\w]*)#?(?:[\.\!\/\\\w]*))?)/g){
-					print MODIFIED "$&\n";
-					$tmp =~ s/https/http/g;
-				}
-				close MODIFIED;
-				
-			#$c->send_basic_header( $response->status_line );
-			#$c->send_file_response( "modified.html" );
-			#$c->send_status_line( "200" );
-			#$c->send_header("Content-Type",$response->header("Content-Type"));
-			#$c->send_header("Content-Length",length($tmp));
-			#$c->send_crlf();
-			#$c->send_response("RESP HIERE");
-				if($response->header("Connection")){
-					print "serveur : ".$response->header("Connection")."\n";
-				}
-				if(!$response->status_line){
-					$response->status_line = "200 OK";
-				}
-				print "COOKIE avant l'envoi : ".$response->header("Set-Cookie")."\n";
-				#print "Content-type : ".$response->header("Content-Type")."\n";
-				#print "Content-Length: ".length($tmp)."\n";
-				#print "Status-Line: ".$response->status_line."\n";
-				#print "Connection: ".$response->header("Connection")."\n";
-				#print $tmp;
-	
-				
-				if($response->header("Set-Cookie")){
-					if($c->send_response($response->status_line."\r\nServer: KIKOULOL/0.0\r\nContent-Type : ".$response->header("Content-Type")
-					."\r\nContent-Length: ".length($tmp)."\r\nSet-Cookie: ".$response->header("Set-Cookie")."\r\nConnection: ".$response->header("Connection")."\r\n\r\n".$tmp)){
-						print "Réponse envoyée au client ac cookie\n";
-					}else{
-						print "\nRéponse non envoyé au client ac cookie\ndernier eval: $?\nerreur : $!";
-					}
+				if($c->send_response($send)){
+					print "Réponse envoyée au client ac cookie\n";
 				}else{
-					if($c->send_response($response->status_line."\r\nServer: KIKOULOL/0.0\r\nContent-Type : ".$response->header("Content-Type")
-					."\r\nContent-Length: ".length($tmp)."\r\nConnection: ".$response->header("Connection")."\r\n\r\n".$tmp)){
-						print "Réponse envoyée au client sans cookie\n";
-					}else{
-						print "\nRéponse non envoyé au client sans cookie\n";
-					}
-				}
+					print "\nRéponse non envoyé au client ac cookie\n";
+				} 
+			}
 		}
 		print "Fin d'envoi au client.\n\n";
 		$c->close;
 		undef($c);	
 		
 		print "En attente d'une requête\n";
+			
 }
 
 
+sub decode_content{
+	my ($tmp) = @_;
+	my $encoding = $tmp->header("Content-Encoding");			
+	if($tmp->decode){
+		print "Contenu bien décodé\n";
+	}else{
+		print "Contenu mal décodé\n";
+	}
+	return ($tmp, $encoding);
+}
+				
+
+sub replace_https{
+	my ($tmp, $response) = @_;
+	open (MODIFIED, ">>modified.html");
+	if(${$tmp->content_ref} =~ /(((https:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-]*)?\??(?:[\-\+=&;%@\.\w]*)#?(?:[\.\!\/\\\w]*))?)/g){
+		print MODIFIED "$&\n";
+		${$tmp->content_ref} =~ s/https/http/g;
+	}
+	close MODIFIED;
+	return $tmp;
+}
+
+sub replace_http{
+	my ($tmp_http) = @_;
+	#open (MODIFIED, ">>modified.html");
+	if(${$tmp_http->content_ref} =~ /(((http:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-]*)?\??(?:[\-\+=&;%@\.\w]*)#?(?:[\.\!\/\\\w]*))?)/g){
+		print MODIFIED "$&\n";
+		${$tmp_http->content_ref} =~ s/http/https/g;
+	}
+	#close MODIFIED;
+	return $tmp_http;
+}
+
+sub pack_response{
+
+	my ($tmp, $response, $encoding) = @_;
+
+	my $header = $response->headers;
+	open(PAPA, ">>tab.txt");
+	print PAPA $header->as_string;
+	close(PAPA);
+	if($header->as_string =~ /\n/){
+		print "SUBSTITUTION DONE\n\n";
+		my $tmp_header = $header->as_string;
+		$tmp_header =~ s/\n/\r\n/g;
+	}
+
+	print "ENCODING IS $encoding\n\n";
+	$tmp->encode($encoding);
+
+	if(!$response->status_line){
+		$response->status_line = "200 OK";
+	}
+	my $send = $response->status_line."\r\n";
+	$send .= $header->as_string;
+	print "HEADER ETC...\n\n$send\n";
+	$send .= "\r\n". $tmp->content;
+
+	return $send;
+}
+
+
+sub redirect_301 {
+	my ($request, $response) = @_;
+	
+	print "Checking new location...\n";
+	
+	open (MODIFIED, ">>tab.txt");
+	$request->uri("");
+	$request->uri($response->header("Location"));
+	
+	print "Renvoi de la requete : 301\n";
+	print MODIFIED "URL envoyé : ".$request->uri."\n";
+	my $cookie = $response->header("Set-Cookie");
+	print MODIFIED "Cookie:\n".$cookie."\n";
+	$request->push_header(Cookie => $cookie);
+
+	if($response->header("Referer")){
+		my $referer = $response->header("Referer");
+		$request->header(Referer => $referer);	
+	}	
+
+	print MODIFIED "Requete transformée : cookie\n";
+	print MODIFIED $request->as_string."\n";
+
+	if($response = $ua->request( $request )){
+		print "Requete 301 : retransmise\n";
+		print "COOKIE dans la boucle code : ".$response->header("Set-Cookie")."\n";
+	}
+	return $response;
+	goto CHECK_CODE;
+}
+
+sub redirect_302{
+	my ($request, $response) = @_;
+	print "Checking new location...\n";
+				
+	open (MODIFIED, ">>tab.txt");
+	$request->uri("");
+	
+	$request->uri($response->header("Location"));
+	print MODIFIED $request->uri->scheme."\n\n";
+	print MODIFIED $request->uri->host."\n\n";
+	print MODIFIED "CODE 302\n\n";
+	print MODIFIED $request->as_string."\n";
+	
+	print "Renvoi de la requete : 302\n";
+	print MODIFIED "URL envoyé : ".$request->uri."\n";
+	my $cookie = $response->header("Set-Cookie");
+	$request->push_header(Cookie => $cookie);
+
+	#if($response->header("Referer")){
+		my $referer = "https://facebook.com";#$response->header("Referer");#"https://facebook.com";#
+		$request->header(Referer => $referer);
+		#print "REFERER : $referer\n";
+	#}
+		#if($referer =~ /http:/){
+		#	$referer =~ s/http/https/;
+		#}
+		#print "REFERER : $referer\n";
+	#	$request->header(Referer => $referer);			
+	#}
+	
+	
+	print MODIFIED "Requete transformée : cookie\n";
+	print MODIFIED $request->as_string."\n";
+
+	if($response = $ua->request( $request )){
+		print "Requete 302 : retransmise\n";		
+		print "COOKIE dans la boucle code : ".$response->header("Set-Cookie")."\n";
+	}
+	return $response;
+	goto CHECK_CODE;
+}
